a31f0fd5c860435b8d66782d7543c458b061842d
sjcho <cho960229@gmail.com>
2026-09-21 15:36:57 +0900

    Add search filter

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -8,7 +8,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/api/", "/auth/"],
+      disallow: ["/api/", "/auth/", "/api/"],
     },
     sitemap: `${SITE_URL}/sitemap.xml`,
   };
