5dd69205aaf5f0205df88c307aac71d48b7f630d
sjcho <cho960229@gmail.com>
2026-09-21 17:21:42 +0900

    Add test path to robots rules

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -8,7 +8,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/api/", "/auth/", "/api/"],
+      disallow: ["/api/", "/auth/", "/test/"],
     },
     sitemap: `${SITE_URL}/sitemap.xml`,
   };
